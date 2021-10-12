e2b0f3fe763543a60333c99e091d02e17fe9faa0
Shirag Cheruvattil <shiragcv@outlook.com>
2021-10-12 12:56:04 +0530

    Package definition updated

--- package.py
+++ package.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import utils
+from rezutils import config
 
 
-__metadata = utils.get_metadata('setup.cfg')
+__metadata = config.get_metadata('setup.cfg')
 
 name = __metadata.get('name')
 
@@ -12,8 +12,6 @@ version = __metadata.get('version')
 description = __metadata.get('version') or ''
 
 build_command = 'pip install --target={install_path} {root}'
-
-tools = ["maya"]
 
 def commands():
     env.PATH.prepend("{root}/bin")
